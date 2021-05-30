0f3efabe717aeffe46e97141696365063653a285
Yash-9 <yashbhawre11@gmail.com>
2021-05-30 17:26:23 +0000

    1st commit from feature2 p1.java changed

--- p1.java
+++ p1.java
@@ -2,6 +2,7 @@ public class p1{
 
     public static void main(strg[]){
         System.out.println("Welcome to M-theta Technology Solutions");
+        System.out.println("From feature2 branch");
     }
 }
 
