9316cecacd5eb49f0ea2ae737f55d165a2f6a8d1
Yash-9 <yashbhawre11@gmail.com>
2021-06-02 16:43:32 +0000

    changes from feature1 p2.java changed

--- p2.java
+++ p2.java
@@ -1,4 +1,8 @@
 public class p2{
+      public void data() {
+       System.out.println("Changes from feature1"); 
+    }
+
       public static void main(strgs[]){
           System.out.println("Added features");
       }
