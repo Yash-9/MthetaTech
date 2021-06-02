999af9ad1dad4bdcfeacbbd3340cf24b9a21df8e
Yash-9 <yashbhawre11@gmail.com>
2021-06-02 16:31:39 +0000

    added features from hotfix

--- p2.java
+++ p2.java
@@ -1,6 +1,8 @@
 public class p2{
       public static void main(strgs[]){
           System.out.println("Added features");
+          System.out.println("Added features from hotfix branch");
+          
       }
 
 }
